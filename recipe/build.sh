4ad4c8c71641fdf37002a72f6bf5eb429048a33d
danielnachun <daniel.nachun@gmail.com>
2024-06-19 21:02:32 -0700

    specify path to liblpeg

--- recipe/build.sh
+++ recipe/build.sh
@@ -12,6 +12,7 @@ cmake -S . -B build \
     -DUSE_BUNDLED=OFF \
     -DLIBLUV_LIBRARY="${PREFIX}/lib/libluv${SHLIB_EXT}" \
     -DLIBUV_LIBRARY="${PREFIX}/lib/libuv${SHLIB_EXT}" \
+    -DLPEG_LIBRARY="${PREFIX}/lib/liblpeg${SHLIB_EXT}" \
     ${CMAKE_ARGS}
 cmake --build build
 cmake --install build
