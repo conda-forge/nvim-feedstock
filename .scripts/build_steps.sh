feeee6c5ff222fad456db24e8f9fa1d58404820c
Daniel Nachun <daniel.nachun@gmail.com>
2025-03-28 00:03:38 -0700

    MNT: Re-rendered with conda-build 25.1.2, conda-smithy 3.47.0, and conda-forge-pinning 2025.03.27.19.44.52

--- .scripts/build_steps.sh
+++ .scripts/build_steps.sh
@@ -54,7 +54,7 @@ ulimit -n 1024
 # "recipe/yum_requirements.txt" file. After updating that file,
 # run "conda smithy rerender" and this line will be updated
 # automatically.
-/usr/bin/sudo -n yum install -y glibc-gconv-extra glibc-all-langpacks
+/usr/bin/sudo -n yum install -y glibc-gconv-extra glibc-locale-source
 )
 
 # make the build number clobber
