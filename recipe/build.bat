d5e2a103652d6869c6f56472c721c50ac5d1a680
UENO, M. <eunosium-1128@live.jp>
2025-09-04 17:33:45 +0900

    Remove UTF-8 handling flags from build.bat

--- recipe/build.bat
+++ recipe/build.bat
@@ -1,23 +1,8 @@
 @echo on
 
-REM Set flags to handle UTF-8 properly
-setlocal enabledelayedexpansion
-
-set LANG=en_US.UTF-8
-set LC_ALL=en_US.UTF-8
-set LC_CTYPE=en_US.UTF-8
-set VSLANG=1033
-set PYTHONUTF8=1
-chcp 65001 > nul 2>&1
-set CL=/utf-8
-set CFLAGS=-D_CRT_SECURE_NO_WARNINGS /utf-8
-set CXXFLAGS=-D_CRT_SECURE_NO_WARNINGS /utf-8
-
 cmake -S cmake.deps -B .deps -G Ninja ^
     -DUSE_BUNDLED_UV=ON ^
     -DUSE_BUNDLED=OFF ^
-    -DCMAKE_C_FLAGS="/utf-8 /D_CRT_SECURE_NO_WARNINGS" ^
-    -DCMAKE_CXX_FLAGS="/utf-8 /D_CRT_SECURE_NO_WARNINGS" ^
     %CMAKE_ARGS% ^
     || goto :error
 
@@ -26,8 +11,6 @@ cmake --build .deps --parallel %CPU_COUNT% || goto :error
 cmake -S . -B build -G Ninja ^
     -DENABLE_TRANSLATIONS=ON ^
     -DUSE_BUNDLED=OFF ^
-    -DCMAKE_C_FLAGS="/utf-8 /D_CRT_SECURE_NO_WARNINGS" ^
-    -DCMAKE_CXX_FLAGS="/utf-8 /D_CRT_SECURE_NO_WARNINGS" ^
     %CMAKE_ARGS% ^
     || goto :error
 
