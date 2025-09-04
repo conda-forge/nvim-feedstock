b457ade01179802c6fd8cc416827ef961aee157d
UENO, M. <eunosium-1128@live.jp>
2025-09-04 13:31:16 +0900

    Update the recipe for Windows

--- recipe/build.bat
+++ recipe/build.bat
@@ -2,13 +2,13 @@
 set "CMAKE_GENERATOR=Ninja"
 
 cmake -S cmake.deps -B .deps -G Ninja -D CMAKE_BUILD_TYPE=Release -DUSE_BUNDLED=OFF -DUSE_BUNDLED_UV=ON || goto :error
-cmake --build .deps || goto :error
+cmake --build .deps -j %CPU_COUNT% || goto :error
 cmake -S . -B build ^
     -G %CMAKE_GENERATOR% ^
     -DCMAKE_BUILD_TYPE=Release ^
     -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
     %CMAKE_ARGS% || goto :error
-cmake --build build --config Release || goto :error
+cmake --build build --config Release -j %CPU_COUNT% || goto :error
 
 @REM cmake --install build fails with permission denied error
 @REM we need to manually copy the files over
@@ -17,6 +17,7 @@ REM copy .\build\windows_runtime_deps\cat.exe %LIBRARY_PREFIX%\bin || goto :erro
 REM copy .\build\windows_runtime_deps\tee.exe %LIBRARY_PREFIX%\bin || goto :error
 copy .\build\windows_runtime_deps\win32yank.exe %LIBRARY_PREFIX%\bin || goto :error
 copy .\build\windows_runtime_deps\xxd.exe %LIBRARY_PREFIX%\bin || goto :error
+xcopy /E /I /Y .\build\runtime\* %LIBRARY_PREFIX%\share\nvim\runtime\ || goto :error
 
 goto :EOF
 
