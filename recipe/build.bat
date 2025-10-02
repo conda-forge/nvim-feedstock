139c6ae44f1e97be572e15ac524a7e730f06b14f
Jan Robert Schmidt <jan_robert.schmidt@tu-dresden.de>
2025-10-02 12:49:03 +0200

    Set `global-ignore-conda-prefix`

--- recipe/build.bat
+++ recipe/build.bat
@@ -17,6 +17,11 @@ cmake -S . -B build -G Ninja ^
 cmake --build build --parallel %CPU_COUNT% || goto :error
 cmake --install build --parallel %CPU_COUNT% || goto :error
 
+:: Tell `pixi global` to not set CONDA_PREFIX during activation
+:: https://pixi.sh/dev/global_tools/introduction/#opt-out-of-conda_prefix
+if not exist "%PREFIX%\\etc\\pixi" mkdir "%PREFIX%\\etc\\pixi\\nvim"
+type nul > "%PREFIX%\\etc\\pixi\\nvim\\global-ignore-conda-prefix"
+
 goto :EOF
 
 :error
